0c3fd431836768631ce8cf628bc1ef171fdedaab
Gerro Den <GerroDen@users.noreply.github.com>
2022-06-08 19:32:22 +0200

    Add stylelint with postcss processing for CSS in JS code

--- web/stylelint.config.js
+++ web/stylelint.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   extends: "stylelint-config-recommended",
+  overrides: [
+    {
+      files: ["**/*.tsx"],
+      customSyntax: "postcss-js",
+    },
+  ],
   rules: {
     "no-empty-source": null,
     "declaration-empty-line-before": null,
