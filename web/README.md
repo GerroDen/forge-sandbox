c85462941950a4de77381af033946ed51934f0e1
Gerro Den <GerroDen@users.noreply.github.com>
2022-08-15 14:09:48 +0200

    Add notes why still using vite2 and swc instead of esbuild for jest

---
+++ web/README.md
@@ -0,0 +1,3 @@
+# Vite
+
+This project runs with vite2 for now, because some plugins are not compatible with vite3 at the moment of this project's creation.
--- web/jest.config.js
+++ web/jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   rootDir: "./src",
   transform: {
+    // "\\.tsx?$": ["esbuild-jest", { sourcemap: true }],
+    // switched to swc until issue with jest.mock is fixed: https://github.com/aelbore/esbuild-jest/issues/54
     "\\.[jt]sx?$": "@swc/jest",
   },
   moduleNameMapper: {
