531fa8c8f5012be68c70817aa4450d82ac351b64
Gerro Den <GerroDen@users.noreply.github.com>
2022-08-17 14:21:34 +0200

    Define process.env.NODE_ENV as it is required by some atlaskit components

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -29,6 +29,9 @@ export default defineConfig(({ mode }) => {
         }),
       },
     },
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    },
     base: "./",
     server: {
       port: env.LOCAL_DEV === "true" ? 3000 : 3001,
