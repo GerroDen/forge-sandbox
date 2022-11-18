7acf19f016d47c6058ad4b58169ab03dabc34d5e
Gerro Den <GerroDen@users.noreply.github.com>
2022-11-18 11:27:36 +0100

    Add product prop to ForgeBrigeLink stories

--- packages/web/src/components/ForgeBridgeLink.stories.tsx
+++ packages/web/src/components/ForgeBridgeLink.stories.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { ComponentMeta } from "@storybook/react";
 import { ForgeBridgeLink } from "@/components/ForgeBridgeLink";
+import { Product } from "@/lib/forge-context";
 
 export default {
   component: ForgeBridgeLink,
 } as ComponentMeta<typeof ForgeBridgeLink>;
 
 export function Example() {
-  return <ForgeBridgeLink href="/somewhere">Click Me!</ForgeBridgeLink>;
+  return (
+    <ForgeBridgeLink href="/somewhere" product={Product.jira}>
+      Click Me!
+    </ForgeBridgeLink>
+  );
 }
 
 export function FullUrl() {
   return (
-    <ForgeBridgeLink href="https://google.com">
+    <ForgeBridgeLink href="https://google.com" product={Product.jira}>
       https://google.com
     </ForgeBridgeLink>
   );
@@ -20,7 +25,7 @@ export function FullUrl() {
 
 export function NewWindow() {
   return (
-    <ForgeBridgeLink href="https://google.com" newWindow>
+    <ForgeBridgeLink href="https://google.com" product={Product.jira} newWindow>
       https://google.com
     </ForgeBridgeLink>
   );
@@ -29,7 +34,11 @@ export function NewWindow() {
 export function OverrideOnClick() {
   const onClick = () => window.alert("Overridden onClick");
   return (
-    <ForgeBridgeLink href="https://google.com" onClick={onClick}>
+    <ForgeBridgeLink
+      href="https://google.com"
+      product={Product.jira}
+      onClick={onClick}
+    >
       https://google.com
     </ForgeBridgeLink>
   );
