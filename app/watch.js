697ce60fde3ba081d126167dbbbf3fed48f7753f
Gerro Den <GerroDen@users.noreply.github.com>
2023-05-04 13:02:11 +0200

    Initially build and catch errors on watch in dev mode

--- app/watch.js
+++ app/watch.js
@@ -3,12 +3,14 @@ import chokidar from "chokidar";
 import { bundle, manifestFile } from "./build.js";
 
 const paths = ["src/**/*.ts", manifestFile, "tsconfig.json", "package.json"];
-await bundle();
+await build();
 console.log(`watching changes on ${paths.join(", ")}`);
-chokidar.watch(paths, { ignoreInitial: true }).on("all", async () => {
+chokidar.watch(paths, { ignoreInitial: true }).on("all", build);
+
+async function build() {
   try {
     await bundle();
   } catch (e) {
     console.error("bundling failed", e);
   }
-});
+}
