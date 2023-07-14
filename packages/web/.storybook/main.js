23b2b9e8ec009ec0a669946443b90ffd3b9ab8f0
Gerro Den <GerroDen@users.noreply.github.com>
2023-07-14 10:34:15 +0200

    Update storybook config

--- packages/web/.storybook/main.js
+++ packages/web/.storybook/main.js
@@ -1,8 +1,23 @@
+const { resolve } = require("path");
+const rootDir = resolve(__dirname, "..");
+const srcDir = resolve(rootDir, "src");
+
 module.exports = {
-  stories: ["../src/**/*.stories.@(j|t)sx"],
+  stories: [
+    resolve(__dirname, "pages/**/*.stories.mdx"),
+    resolve(srcDir, "**/*.stories.@(j|t)sx"),
+  ],
+  staticDirs: [resolve(__dirname, "public")],
   addons: ["@storybook/addon-essentials"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  docs: {
+    autodocs: true,
+    docsMode: true,
+  },
+  typescript: {
+    skipBabel: true,
   },
 };
